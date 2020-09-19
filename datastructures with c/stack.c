248e1ce5dad0e27452f8e67199c0b06b8af8cf6e
omsatam <62635289+omsatam@users.noreply.github.com>
2020-09-19 17:18:20 +0530

    added stack

---
+++ datastructures with c/stack.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+    int size;
+    int top;
+    int *arr;
+};
+int isempty(struct stack * s){
+    if(s->top == -1){
+        return 1;
+    }
+    else { return 0; }
+}
+int isfull(struct stack * s){
+    if(s->top == s->size - 1){
+        return 1;
+    }
+    else { return 0; }
+}
+void push(struct stack * s,int element){
+    // int value;
+    if(isfull(s)){
+        printf("The stack is already full\n");
+    }
+    else{
+        
+        s->top++;
+        s->arr[s->top] = element;
+        // return value;
+    }
+}
+int pop(struct stack * s){
+    int value;
+      if(isempty(s)){
+        printf("The stack is empty\n");
+    }
+    else{
+       value = s->arr[s->top];
+        s->top--;
+        return value;
+    }
+}
+int peek(struct stack *s){
+    return s->arr[s->top];    
+}
+void show_stack(struct stack *s){
+    int a = s->top;
+    printf("\n");
+    do{
+        printf("%d\n",s->arr[a]);
+        a--;
+    }while(a>=0);
+    
+}
+
+int main()
+{
+    struct stack s;
+    s.size = 50;
+    s.top = -1;
+    s.arr = (int *)malloc(s.size * sizeof(int));
+    push(&s,45);
+    push(&s,46);
+    push(&s,456);
+    push(&s,457);
+    push(&s,39);
+    push(&s,37);
+    push(&s,451);
+    push(&s,452);
+    push(&s,45);
+    push(&s,46);
+    push(&s,456);
+    push(&s,457);
+    push(&s,39);
+    push(&s,37);
+    push(&s,451);
+    push(&s,452);
+    printf("%d ",pop(&s));
+    printf("%d ",pop(&s));
+    printf("%d ",pop(&s));
+    printf("%d ",pop(&s));
+    printf("%d ",pop(&s));
+    
+
+    show_stack(&s);
+    printf("%d ",peek(&s));
+
+    return 0;
+}
